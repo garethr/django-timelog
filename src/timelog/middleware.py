052a4040f72b9ee8f04af9e79a9061f8a0122890
Ross Poulton <ross@rossp.org>
2011-06-15 07:41:25 +0000

    Add support for Python versions prior to 2.6 by using old-syntax string formatting.

--- src/timelog/middleware.py
+++ src/timelog/middleware.py
@@ -14,6 +14,6 @@ class TimeLogMiddleware(object):
         d = {'method': request.method, 'time': time.time() - request._start,
              'code': response.status_code,
              'url': smart_str(request.path_info)}
-        msg = '{method} "{url}" ({code}) {time:.2f}'.format(**d)
+        msg = '%(method)s "%(url)s" (%(code)s) %(time).2f' % d
         logger.info(msg)
         return response
