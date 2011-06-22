8a40ec744c8c5fa011630fe138cdc2b8b3c88fd0
Gareth Rushgrove <gareth@morethanseven.net>
2011-06-22 20:07:05 +0100

    dump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "django-timelog",
-    version = "0.1",
+    version = "0.2",
     author = "Gareth Rushgrove",
     author_email = "gareth@morethanseven.net",
     url = "http://github.com/garethr/django-timelog/",
